9d6f9f56c43e83691ccdc50474826621fa116765
conorburke <cjburke89@gmail.com>
2016-12-06 21:06:04 -0800

    add styles to weather components

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -10,7 +10,15 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p className="text-center">My React Weather App</p>
+      <p className="text-center">Here are some resources</p>
+      <ul className="text-center">
+        <li><a href="https://facebook.github.io/react">React</a></li>
+        <li><a href="http://openweathermap.org">Weather Map</a></li>
+      </ul>
+    </div>
   )
 };
 
