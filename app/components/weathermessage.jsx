ee29f0e57f7f18e90b64ec717e48eda463d9986b
conorburke <cjburke89@gmail.com>
2016-12-10 12:28:09 -0800

    adjust output and fix error that disabled future searches after searching for unknown place

--- app/components/weathermessage.jsx
+++ app/components/weathermessage.jsx
@@ -19,7 +19,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
     return (
-      <h3 className="text-center">It is {temp} in {location}</h3>
+      <h3 className="text-center">It is {temp} &#x2109; in {location}</h3>
     )
 };
 
